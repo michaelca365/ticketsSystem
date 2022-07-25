7f6e50c177d966880fd3dd3c01c781a683627f3f
Steve Guevara <michaelito3d@gmail.com>
2022-07-24 20:19:53 -0500

    make it better

--- database/config.ts
+++ database/config.ts
@@ -1,13 +1,14 @@
-import { Sequelize, Options } from 'sequelize';
+import { Sequelize, Options } from "sequelize";
 
 const options: Options = {
   dialect: "postgres",
   logging: process.env.NODE_ENV === "development" ? (str) => console.log(str) : false,
   dialectOptions: {
-    require: true,
-    rejectUnauthorized: false
-  }
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 };
-
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL || "", options);
--- database/dbConfig.js
+++ database/dbConfig.js
@@ -11,8 +11,10 @@ module.exports = {
       url: process.env.DATABASE_URL,
       dialect: 'postgres',
       dialectOptions: {
-        require: false,
-        ssl: true
-      }
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
     }
   }
