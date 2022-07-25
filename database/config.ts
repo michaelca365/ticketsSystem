ea66513e3261551a3c2cbeea325f88319902543e
Steve Guevara <michaelito3d@gmail.com>
2022-07-24 23:11:35 -0500

    fix

--- database/config.ts
+++ database/config.ts
@@ -3,7 +3,7 @@ import { Sequelize, Options } from "sequelize";
 const options: Options = {
   dialect: "postgres",
   logging: process.env.NODE_ENV === "development" ? (str) => console.log(str) : false,
-  dialectOptions: {
+  dialectOptions: process.env.NODE_ENV === "development" ? undefined : {
     ssl: {
       require: true,
       rejectUnauthorized: false,
