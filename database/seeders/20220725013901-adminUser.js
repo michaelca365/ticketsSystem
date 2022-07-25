9a49aef63e4b10437037dd2f08f5b0db6c998855
Steve Guevara <michaelito3d@gmail.com>
2022-07-24 20:53:49 -0500

    finish

--- database/seeders/20220725013901-adminUser.js
+++ database/seeders/20220725013901-adminUser.js
@@ -6,6 +6,7 @@ module.exports = {
       USER_TABLE,
       [
         {
+          id: "c95a223b-664e-44fa-a3ea-6b0e32be954e",
           email: "example@example",
           password: "$2a$10$fcflreKgc9uJ3xeA.bqGCusx81jbf23xTRFwhoWL1HAgGENQvDvqy",
           user_name: "Jhon Doe",
